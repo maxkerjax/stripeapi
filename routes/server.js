199a4835a564760e7cdc70091eacf626b808bc38
DESKTOP-01OV9TN\Baifern <maxkerjax@gmail.com>
2025-06-23 21:35:08 +0700

    my first commit

--- routes/server.js
+++ routes/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controllers/userController');
+
 const stripeController = require('../controllers/stripeController');
-const reportController = require('../controllers/reportController');
+// const userController = require('../controllers/userController');
+// const reportController = require('../controllers/reportController');
 
 router.post('/create-checkout-session', stripeController.createPaymentSession);
-router.post('/loginUser', userController.loginUser);
-router.get('/getReportData', reportController.getReportData);
+// router.post('/loginUser', userController.loginUser);
+// router.get('/getReportData', reportController.getReportData);
 
 module.exports = router;
