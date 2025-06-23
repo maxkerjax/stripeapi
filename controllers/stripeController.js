ad9e17b3f29463f00f46750bb33347cb330d4dc6
DESKTOP-01OV9TN\Baifern <maxkerjax@gmail.com>
2025-06-23 20:33:28 +0700

    my first commit

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -38,8 +38,8 @@ exports.createPaymentSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `${process.env.VITE_APP_URL  || 'https://donimigemini.vercel.app/'}/billing?success=true&billing_id=${billingId}`,
-      cancel_url: `${process.env.VITE_APP_URL || 'https://donimigemini.vercel.app/'}/billing?canceled=true`,
+      success_url: `${process.env.VITE_APP_URL  || 'https://donimigemini.vercel.app'}/billing?success=true&billing_id=${billingId}`,
+      cancel_url: `${process.env.VITE_APP_URL || 'https://donimigemini.vercel.app'}/billing?canceled=true`,
       metadata: {
         billingId: billingId.toString(),
       },
